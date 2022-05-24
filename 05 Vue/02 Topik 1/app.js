e3199ed348cf2a139ae05c77295ad038a31dfae0
HamsterKaget <radzaramdani21@gmail.com>
2022-05-25 01:47:46 +0700

    Vue.js

---
+++ 05 Vue/02 Topik 1/app.js
@@ -0,0 +1,11 @@
+var product = {
+  name: "Server Discord Comunity Edition",
+  description: "Server Untuk Komunitas",
+  price: "Rp.40.000",
+  image: "http://hamsterkaget.skom.id/b4c.gif",
+};
+
+var app = new Vue({
+  el: "#app",
+  data: product,
+});
