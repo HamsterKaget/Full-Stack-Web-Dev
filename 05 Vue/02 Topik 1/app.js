cf7e379becc2c0efe1023b3d44d637bf217432dd
HamsterKaget <radzaramdani21@gmail.com>
2022-05-26 11:01:36 +0700

    Chapter 1 Complete

--- 05 Vue/02 Topik 1/app.js
+++ 05 Vue/02 Topik 1/app.js
@@ -1,34 +1,20 @@
-var dataProduct = {
-  maximum: 50000,
-  products: [
-    {
-      id: "429",
-      name: "Server Discord Comunity Edition",
-      description:
-        "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum minima inventore est pariatur explicabo similique cum. Culpa, inventore! Dolores ipsam repudiandae alias est autem ex beatae, modi commodi veniam quae.",
-      price: "40000",
-      image: "../../image/Listing.png",
-    },
-    {
-      id: "430",
-      name: "Server Discord Shop Edition",
-      description:
-        "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum minima inventore est pariatur explicabo similique cum. Culpa, inventore! Dolores ipsam repudiandae alias est autem ex beatae, modi commodi veniam quae.",
-      price: "45000",
-      image: "../../image/Server Discord Shop.png",
-    },
-    {
-      id: "431",
-      name: "Server Discord Youtube Edition",
-      description:
-        "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum minima inventore est pariatur explicabo similique cum. Culpa, inventore! Dolores ipsam repudiandae alias est autem ex beatae, modi commodi veniam quae.",
-      price: "55000",
-      image: "../../image/Server Discord Youtube.png",
-    },
-  ],
-};
-
 var app = new Vue({
   el: "#app",
-  data: dataProduct,
+  data: {
+    maximum: 50,
+    products: null,
+    cart: [],
+  },
+  mounted: function () {
+    fetch("https://hplussport.com/api/products/order/price")
+      .then((response) => response.json())
+      .then((data) => {
+        this.products = data;
+      });
+  },
+  methods: {
+    addItem: function (products) {
+      this.cart.push(products);
+    },
+  },
 });
