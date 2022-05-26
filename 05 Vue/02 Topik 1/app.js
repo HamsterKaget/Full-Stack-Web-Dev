d44becd0e99a6965c23b0fa744c401fbc2892e84
HamsterKaget <radzaramdani21@gmail.com>
2022-05-26 10:07:23 +0700

    looping & condition

--- 05 Vue/02 Topik 1/app.js
+++ 05 Vue/02 Topik 1/app.js
@@ -1,11 +1,34 @@
-var product = {
-  name: "Server Discord Comunity Edition",
-  description: "Server Untuk Komunitas",
-  price: "Rp.40.000",
-  image: "http://hamsterkaget.skom.id/b4c.gif",
+var dataProduct = {
+  maximum: 50000,
+  products: [
+    {
+      id: "429",
+      name: "Server Discord Comunity Edition",
+      description:
+        "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum minima inventore est pariatur explicabo similique cum. Culpa, inventore! Dolores ipsam repudiandae alias est autem ex beatae, modi commodi veniam quae.",
+      price: "40000",
+      image: "../../image/Listing.png",
+    },
+    {
+      id: "430",
+      name: "Server Discord Shop Edition",
+      description:
+        "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum minima inventore est pariatur explicabo similique cum. Culpa, inventore! Dolores ipsam repudiandae alias est autem ex beatae, modi commodi veniam quae.",
+      price: "45000",
+      image: "../../image/Server Discord Shop.png",
+    },
+    {
+      id: "431",
+      name: "Server Discord Youtube Edition",
+      description:
+        "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum minima inventore est pariatur explicabo similique cum. Culpa, inventore! Dolores ipsam repudiandae alias est autem ex beatae, modi commodi veniam quae.",
+      price: "55000",
+      image: "../../image/Server Discord Youtube.png",
+    },
+  ],
 };
 
 var app = new Vue({
   el: "#app",
-  data: product,
+  data: dataProduct,
 });
